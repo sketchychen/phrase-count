b3199b38d46359cd20249027450e3d4129241300
skylocke <mathematical.fiction@gmail.com>
2017-03-30 11:04:08 -0700

    updated readme.md with game plan, working on reducing string search

--- phrase_count.py
+++ phrase_count.py
@@ -6,10 +6,12 @@ import re, string
 filename = "samuel_l_jackson.txt"
 file_ = open(filename)
 
+# GENERATE LIST OF SENTENCES (phrases do not span sentences)
 def split_sentences(ss):
     return re.split("(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s", ss.strip()) # .strip() removes line breaks
     # in future consider how to check for "Mrs." as adding (?<!(Mrs)\.) creates a NoneType
 
+# CONVERT TXT FILE TO LIST OF STRINGS (that are sentences)
 def file_sentence_list(ff):
     sentence_list = []
     for line in ff:
@@ -17,11 +19,13 @@ def file_sentence_list(ff):
             sentence_list.extend(split_sentences(line))
     return [strip_punctuation(sentence).lower() for sentence in sentence_list]
 
+# REMOVE PUNCTUATION FOR BETTER DICTIONARY WORD COUNT
 def strip_punctuation(ss):
     # future: consider addressing contractions and possessives
     punctuation = ",:.?!"
     return ''.join(char for char in ss if char not in punctuation)
 
+# COUNT OCCURRENCE OF WORDS (as phrases are generally made up of words)
 def count_words(ss):
     word_count = defaultdict(int)
     for word in ss.split():
@@ -29,9 +33,23 @@ def count_words(ss):
     return word_count
 
 sentences = file_sentence_list(file_)
+
+# sentences is a list of strings while file_ is a file object, so might as well
+# use what's already got strings instead of converting file_ to string again
+# to get the list of words
 words = count_words(" ".join(sentences))
 
+# WORDS THAT ONLY APPEAR ONCE IMPLY THAT THE PHRASES THEY'RE IN ALSO OCCUR ONCE
+single_occurrence_words = [word_count[0] for word_count in words.items() if word_count[1] == 1] # words.items() returns list of tuples
 
-print(sentences)
-print(sorted(words.items(), reverse=True, key=itemgetter(1)))
+# SPLIT SENTENCES INTO FRAGMENTS BASED ON SINGLE-OCCURRING WORDS
+fragments = 
+
+# IGNORE FRAGMENTS WITH WORD LENGTH < 3 WORDS
+fragments = [fragment for fragment in fragments if len(fragment.split()) < 3]
+
+
+# print(sentences)
+# print(sorted(words.items(), reverse=True, key=itemgetter(1)))
 # print(string.punctuation)
+# print([word_count[0] for word_count in words.items() if word_count[1] == 1])
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
+## challenge:
+
 Given a string representing a document, write a function which returns the top 10 most frequent repeated phrases. A phrase is a stretch of three to ten consecutive words and cannot span sentences. Only include a phrase if it is not a subset of another, longer phrase (if “calm cool” and “calm cool and collected” are repeated, do not include “calm cool” in the returned set).
+
+## current game plan:
+- convert txt file to list of strings (sentence)
+- make everything lowercase and remove punctuation from words
+- count frequency of words (removing punctuation will allow "human," to count as "human", "help." as "help", and etc.)
+- use one-time-occurring words to eliminate one-time-occurring phrases (to reduce what we have to search through)
+- find some way to iterate through remaining fragments for phrases taking into account subsets
